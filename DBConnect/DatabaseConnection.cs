3150384e3dab39f2bc2ac304f32eb197654b77fa
Muddathir Mathure <mathurem@hotmail.com>
2024-09-12 13:38:23 +0400

    Microsoft.Data.SqlClient updated to latest version

---
+++ DBConnect/DatabaseConnection.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Mexico_Utility.DBConnect
+{
+    public class DatabaseConnection
+    {
+        private readonly string connectionString;
+
+        public DatabaseConnection(string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                {
+                    throw new ArgumentNullException(nameof(connectionString), "The connection string cannot be null or empty.");
+                }
+            }
+            this.connectionString = connectionString;
+        }
+
+        public async Task<SqlConnection> GetConnectionAsync()
+        {
+            var connection = new SqlConnection(connectionString);
+            await connection.OpenAsync();
+            return connection;
+        }
+    }
+}
--- ExcludeParameterAttribute.cs
+++ ExcludeParameterAttribute.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Mexico_Utility
 {
         [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
