4cb346bdc530df9ea23acc0654d44e3af6e68a20
Muddathir Mathure <mathurem@hotmail.com>
2024-09-12 13:50:04 +0400

    Utils class added

---
+++ Utils/Utils.cs
@@ -0,0 +1,11 @@
+﻿namespace Mexico_Utility.Utils
+{
+    public class Utils
+    {
+        [ExcludeParameter]
+        public string message { get; set; } = string.Empty;
+        [ExcludeParameter]
+
+        public string results { get; set; } = string.Empty;
+    }
+}
